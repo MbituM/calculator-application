71b2752926d3a4e5a783e6fb70a207269fc1312a
Mbue <93818178+MbituM@users.noreply.github.com>
2021-12-14 22:53:34 +0300

    Add files via upload

---
+++ calculator/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmcalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btntotal = New System.Windows.Forms.Button()
+        Me.txttotal = New System.Windows.Forms.TextBox()
+        Me.txtnum2 = New System.Windows.Forms.TextBox()
+        Me.txtnum1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btntotal
+        '
+        Me.btntotal.Location = New System.Drawing.Point(331, 254)
+        Me.btntotal.Name = "btntotal"
+        Me.btntotal.Size = New System.Drawing.Size(75, 23)
+        Me.btntotal.TabIndex = 0
+        Me.btntotal.Text = "Total"
+        Me.btntotal.UseVisualStyleBackColor = True
+        '
+        'txttotal
+        '
+        Me.txttotal.Location = New System.Drawing.Point(306, 193)
+        Me.txttotal.Name = "txttotal"
+        Me.txttotal.Size = New System.Drawing.Size(100, 20)
+        Me.txttotal.TabIndex = 1
+        '
+        'txtnum2
+        '
+        Me.txtnum2.Location = New System.Drawing.Point(306, 108)
+        Me.txtnum2.Name = "txtnum2"
+        Me.txtnum2.Size = New System.Drawing.Size(100, 20)
+        Me.txtnum2.TabIndex = 2
+        '
+        'txtnum1
+        '
+        Me.txtnum1.Location = New System.Drawing.Point(306, 51)
+        Me.txtnum1.Name = "txtnum1"
+        Me.txtnum1.Size = New System.Drawing.Size(100, 20)
+        Me.txtnum1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(246, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Number 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(246, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Number 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(260, 193)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(31, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Total"
+        '
+        'frmcalculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtnum1)
+        Me.Controls.Add(Me.txtnum2)
+        Me.Controls.Add(Me.txttotal)
+        Me.Controls.Add(Me.btntotal)
+        Me.Name = "frmcalculator"
+        Me.Text = "calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btntotal As Windows.Forms.Button
+    Friend WithEvents txttotal As Windows.Forms.TextBox
+    Friend WithEvents txtnum2 As Windows.Forms.TextBox
+    Friend WithEvents txtnum1 As Windows.Forms.TextBox
+    Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents Label2 As Windows.Forms.Label
+    Friend WithEvents Label3 As Windows.Forms.Label
+End Class
---
+++ calculator/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmcalculator
+
+    Private Sub btntotal_Click(sender As Object, e As EventArgs) Handles btntotal.Click
+        txttotal.Text = Val(txtnum1.Text) + Val(txtnum2.Text)
+    End Sub
+End Class
